0851c3817f93b8d8a3e97d45a32a6ba01aeebfb1
abderrahim <info.abderrahimtijani@gmail.com>
2021-01-25 17:32:05 +0100

    add icon to hide the pop up

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const mybtn = document.getElementById('btn');
+const hidPopup = document.getElementById('hidepopup');
 const videoBox = document.getElementById('videobox');
 
 
 mybtn.addEventListener('click', ()=> {
     videoBox.style.display = "flex";
+
+})
+
+hidPopup.addEventListener('click', ()=> {
+    videoBox.style.display = "none";
 
 })
 
