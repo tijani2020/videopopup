82e08911545cfcbecc8b0bc57db6babc8d7699cc
abderrahim <info.abderrahimtijani@gmail.com>
2021-01-25 17:23:29 +0100

    frst commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const mybtn = document.getElementById('btn');
+const videoBox = document.getElementById('videobox');
+
+
+mybtn.addEventListener('click', ()=> {
+    videoBox.style.display = "flex";
+
+})
+
